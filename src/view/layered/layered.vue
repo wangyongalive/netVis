c456df53a4539d4a4c398275c80856d9b576b3eb
wangyongaliveuser.email <jsonwang0102@163.com>
2019-03-03 22:49:27 +0800

    ''

--- src/view/layered/layered.vue
+++ src/view/layered/layered.vue
@@ -4,11 +4,12 @@
 </template>
 
 <script>
+  import axios from 'axios'
+  import qs from 'qs'
   import echarts from 'echarts';
   import 'echarts-gl';
   // import data from '../../../static/mock/table.json';
   import data from '../../../static/mock/node.json';
-  // import data from '../../../static/mock/test.json';
 
   export default {
     name: "layered",
@@ -18,6 +19,7 @@
     methods: {
       init() {
         // 基于准备好的dom，初始化echarts实例
+        let self = this;
         var myChart = echarts.init(document.getElementById('container'));
         myChart.setOption({
           tooltip: {
@@ -25,36 +27,30 @@
               let res = ''
               res += `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;background-color:${datas.color};"></span><br>`
               for (let i in datas.data) {
-                if(['x','y','level'].includes(i))
-                  res += `-  ${i} : ${datas.data[i]}<br>`
+                if (['id', 'level'].includes(i))
+                  res += `-  ${i} : ${datas.data[i]}<br>`;
               }
-              return res
+              return res;
             }
           },
           // 需要注意的是我们不能跟 grid 一样省略 grid3D
-          grid3D:
-            {
-              viewControl: {
-                // 使用正交投影。
-                projection: 'orthographic'
-              }
-              ,
-              // width: '50%',
-              show: false
-            }
-          ,
+          grid3D: {
+            viewControl: {
+              // 使用正交投影。
+              projection: 'orthographic'
+            },
+            // width: '50%',
+            show: false
+          },
           xAxis3D: {
             type: 'value'
-          }
-          ,
+          },
           yAxis3D: {
             type: 'value'
-          }
-          ,
+          },
           zAxis3D: {
             type: 'value'
-          }
-          ,
+          },
           visualMap: { // 将第4个维度用颜色编码
             type: 'continuous',
             min: 1,
@@ -67,7 +63,8 @@
               color: '#fff'
             },
             inRange: {
-              color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090']
+              color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090'],
+              symbolSize: [1, 7]
             }
           },
           dataset: {
@@ -86,9 +83,17 @@
             }
           ]
         })
-
         myChart.on('click', function (params) {
-          console.log(params);
+          let showItems = self.$store.state.nodesLinks.nodes.filter((item) => item.level === params.data.level ? true : false);
+          let showIds = showItems.map((item) => item.id);
+          let param = qs.stringify({
+            data: JSON.stringify(showIds)
+          })
+          axios.post('/get/division', param).then((response) => {
+            // this.nodes = response.data.nodes
+            // this.links = response.data.links
+            // this.renderChart()
+          })
         })
       }
     },
