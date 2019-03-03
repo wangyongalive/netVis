7e3cd148b173e88c55a8b8f768bf06b6b7362142
wangyongaliveuser.email <jsonwang0102@163.com>
2019-03-03 21:23:41 +0800

    ''

--- src/view/layered/layered.vue
+++ src/view/layered/layered.vue
@@ -6,8 +6,8 @@
 <script>
   import echarts from 'echarts';
   import 'echarts-gl';
-  import data from '../../../static/mock/table.json';
-  // import data from '../../../static/mock/node.json';
+  // import data from '../../../static/mock/table.json';
+  import data from '../../../static/mock/node.json';
   // import data from '../../../static/mock/test.json';
 
   export default {
@@ -19,56 +19,55 @@
       init() {
         // 基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('container'));
-        var sizeValue = '57%';
         myChart.setOption({
-          tooltip: {},
-          grid3D: {
-            viewControl: {
-              // 使用正交投影。
-              projection: 'orthographic'
-            },
-            width: '50%',
-            show:false
+          tooltip: {
+            formatter: function (datas) {
+              let res = ''
+              res += `<span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;background-color:${datas.color};"></span><br>`
+              for (let i in datas.data) {
+                if(['x','y','level'].includes(i))
+                  res += `-  ${i} : ${datas.data[i]}<br>`
+              }
+              return res
+            }
           },
-          grid: [
-            {left: '50%', width: '20%', bottom: sizeValue},
-            {left: '75%', width: '20%', bottom: sizeValue},
-            {left: '50%', width: '20%', top: sizeValue},
-            {left: '75%', width: '20%', top: sizeValue}
-          ],
-          xAxis: [
-            {type: 'value', gridIndex: 0, name: 'Income', axisLabel: {rotate: 50, interval: 0}},
+          // 需要注意的是我们不能跟 grid 一样省略 grid3D
+          grid3D:
             {
-              type: 'category',
-              gridIndex: 1,
-              name: 'Country',
-              boundaryGap: false,
-              axisLabel: {rotate: 50, interval: 0}
-            },
-            {type: 'value', gridIndex: 2, name: 'Income', axisLabel: {rotate: 50, interval: 0}},
-            {type: 'value', gridIndex: 3, name: 'Life Expectancy', axisLabel: {rotate: 50, interval: 0}}
-          ],
-          yAxis: [
-            {type: 'value', gridIndex: 0, name: 'Life Expectancy'},
-            {type: 'value', gridIndex: 1, name: 'Income'},
-            {type: 'value', gridIndex: 2, name: 'Population'},
-            {type: 'value', gridIndex: 3, name: 'Population'}
-          ],
+              viewControl: {
+                // 使用正交投影。
+                projection: 'orthographic'
+              }
+              ,
+              // width: '50%',
+              show: false
+            }
+          ,
           xAxis3D: {
-            // 因为 x 轴和 y 轴都是类目数据，所以需要设置 type: 'category' 保证正确显示数据。
-            type: 'category'
-          },
+            type: 'value'
+          }
+          ,
           yAxis3D: {
-            type: 'log'
-          },
-          zAxis3D: {},
+            type: 'value'
+          }
+          ,
+          zAxis3D: {
+            type: 'value'
+          }
+          ,
           visualMap: { // 将第4个维度用颜色编码
+            type: 'continuous',
+            min: 1,
+            max: 7,
+            text:
+              ['顶层', '底层'],
             calculable: true,
-            max: 100,
-            // 维度的名字默认就是表头的属性名
-            dimension: 'Life Expectancy',
+            dimension: 'level',
+            textStyle: {
+              color: '#fff'
+            },
             inRange: {
-              color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+              color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090']
             }
           },
           dataset: {
@@ -77,108 +76,16 @@
           series: [
             {
               type: 'scatter3D',
-              symbolSize: 3,
+              symbolSize: 8,
+              dimensions: ['x', 'y', 'level'],
               encode: {
-                // 维度的名字默认就是表头的属性名
-                x: 'Country',
-                y: 'Population',
-                z: 'Income',
-                tooltip: [0, 1, 2, 3, 4]
-              }
-            },
-            {
-              type: 'scatter',
-              symbolSize: 2.5,
-              xAxisIndex: 0,
-              yAxisIndex: 0,
-              encode: {
-                x: 'Income',
-                y: 'Life Expectancy',
-                tooltip: [0, 1, 2, 3, 4]
-              }
-            },
-            {
-              type: 'scatter',
-              symbolSize: 2.5,
-              xAxisIndex: 1,
-              yAxisIndex: 1,
-              encode: {
-                x: 'Country',
-                y: 'Income',
-                tooltip: [0, 1, 2, 3, 4]
-              }
-            },
-            {
-              type: 'scatter',
-              symbolSize: 2.5,
-              xAxisIndex: 2,
-              yAxisIndex: 2,
-              encode: {
-                x: 'Income',
-                y: 'Population',
-                tooltip: [0, 1, 2, 3, 4]
-              }
-            },
-            {
-              type: 'scatter',
-              symbolSize: 2.5,
-              xAxisIndex: 3,
-              yAxisIndex: 3,
-              encode: {
-                x: 'Life Expectancy',
-                y: 'Population',
-                tooltip: [0, 1, 2, 3, 4]
+                x: 'x',
+                y: 'y',
+                z: 'level'
               }
             }
           ]
         })
-
-        // myChart.setOption({
-        //   tooltip: {},
-        //   // 需要注意的是我们不能跟 grid 一样省略 grid3D
-        //   grid3D: {
-        //     viewControl: {
-        //       // 使用正交投影。
-        //       projection: 'orthographic'
-        //     },
-        //     width: '50%',
-        //     show: false
-        //   },
-        //   xAxis3D: {
-        //     type: 'value'
-        //   },
-        //   yAxis3D: {
-        //     type: 'value'
-        //   },
-        //   zAxis3D: {
-        //     type: 'value'
-        //   },
-        //   visualMap: { // 将第4个维度用颜色编码
-        //     calculable: true,
-        //     // 维度的名字默认就是表头的属性名
-        //     max: 88,
-        //     dimension: 'degree',
-        //     inRange: {
-        //       color: ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
-        //     }
-        //   },
-        //   dataset: {
-        //     source: data
-        //   },
-        //   series: [
-        //     {
-        //       type: 'scatter3D',
-        //       symbolSize: 5,
-        //       encode: {
-        //         // 维度的名字默认就是表头的属性名
-        //         x: 'px',
-        //         y: 'py',
-        //         z: 'level'
-        //       },
-        //       tooltip: ['level']
-        //     }
-        //   ]
-        // })
 
         myChart.on('click', function (params) {
           console.log(params);
